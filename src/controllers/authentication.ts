b9a2c07e7f5a611da8ba09af3d2c1f1583c7ce6d
Rajat321singh <forselfrajat58615@gmail.com>
2024-05-28 16:38:26 +0530

    first commit

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,38 @@ import { getUserByEmail } from "db/users";
 import { random } from "helpers";
 import { createUser } from "db/users";
 import { authentication } from "helpers";
+
+export const login = async (req: express.Request, res: express.Response) =>{
+    try {
+        const {email,password}=req.body;
+        if(!email ||!password){
+            return res.sendStatus(400);
+        }
+        const user=await getUserByEmail(email).Select('+authentication.salt +authentication.password')
+
+        if(!user){
+            return res.sendStatus(400)
+        }
+        const expectedHash=authentication(user.authentication.salt,password)
+        if (user.authentication.passwor!=expectedHash){
+            return res.sendStatus(403);
+        }
+        const salt=random();
+        user.authentication.sessionToken=authentication(salt,user._id.toString());
+        await user.save();
+        res.cookie('NUMERO_WEB',user.authentication.sessionToken,{domain:'localhost',path:'/'})
+        return res.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400)
+        
+    }
+}
+
+
+
+
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
